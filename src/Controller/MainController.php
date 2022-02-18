65116bb808959d9dcf744da69ecc0d214421e1c1
Oleksandr <bondarenkoov1998@gmail.com>
2022-02-18 13:13:42 +0100

    add show by id

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: 'main')]
+#[Route('/', name: 'main.')]
 class MainController extends AbstractController
 {
 
@@ -36,9 +36,18 @@ class MainController extends AbstractController
     public function Get(TestRepository $testRepository)
     {
         $posts = $testRepository->findAll();
-        dump($posts);
-        /*return $this->render('main/index.html.twig',[
+        //dump($posts);
+        return $this->render('main/index.html.twig',[
             'posts' => $posts,
-        ]);*/
+        ]);
+    }
+
+    #[Route('/show/{id}',name: "show")]
+    public function show($id , TestRepository $testRepository)
+    {
+        $post = $testRepository->find($id);
+        return $this->render('show/show.html.twig',[
+            'post' => $post,
+        ]);
     }
 }
